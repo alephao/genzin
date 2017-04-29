4b2be31e3a29d04ad6aefcd286cdeb6dfcd4f482
Aleph Retamal <retamal.ph@gmail.com>
2017-04-30 02:45:17 +1000

    More control and less generics

--- lib/genzin/templates/CellViewModelSnippet.swift
+++
@@ -1,6 +0,0 @@
-protocoloutput:
-    var ___PROPERTYNAME___: ___PROPERTYTYPE___ { get }
-celloutput:
-    let ___PROPERTYNAME___: ___PROPERTYTYPE___
-initialization:
-        self.___PROPERTYNAME___ = ___PROPERTYINITPLACEHOLDER___
